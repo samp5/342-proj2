7500c3ef2162e1e4a669815bbd92ac56e3cb4aa9
moltm3 <moltm3@uic.edu>
2025-03-06 16:04:05 -0600

    forgot to add old changes

--- src/main/java/views/ThreeDayScene.java
+++ src/main/java/views/ThreeDayScene.java
@@ -2,21 +2,55 @@ package views;
 
 import java.util.ArrayList;
 import java.util.Collection;
+
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import my_weather.HourlyPeriod;
-//import views.components.DaySummary;
+import views.components.sidebar.Sidebar;
 
 public class ThreeDayScene {
-  Integer focusedDay = 0;
-  //DaySummary threeDays;
+  Sidebar sidebar;
+  VBox sidebarBox;
+  VBox mainView;
+  HBox sceneBox;
+  Scene scene;
+
+  public ThreeDayScene(ArrayList<HourlyPeriod> forecast) {
+    initComponents();
+    styleComponents();
+  }
 
   private void initComponents() {
+    sidebarBox = new VBox(); // mostly ignored for now
+    mainView = new VBox();
+    sceneBox = new HBox(sidebarBox, mainView);
+    scene = new Scene(sceneBox, 1440, 1024);
   }
 
-  private <T extends Collection<HourlyPeriod>> void initialized_days(T data) {
-    // threeDays = new N_DaySummary(3, data);
+
+  public Scene getScene() {
+    return scene;
+  }
+  
+  public void setSidebar(Sidebar sidebar) {
+    this.sidebar = sidebar;
   }
 
-  public <T extends Collection<HourlyPeriod>> ThreeDayScene(T data) {
-    initialized_days(data);
+  public void setActiveScene() {
+    this.sidebarBox.getChildren().add(sidebar.component());
+  }
+
+  private void styleComponents() {
+    // CONTAINERS
+    // - the sidebar
+    sidebarBox.setMinWidth(256);
+    sidebarBox.setStyle("-fx-background-color: #D9D9D9");
+    // - main view
+    mainView.setMinWidth(1440 - 256);
+    mainView.setStyle("-fx-background-color: #FFFFFF");
+    mainView.setPadding(new Insets(20));
+
   }
 }
