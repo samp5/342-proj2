63a2808cfea5090970fc966d890b09f3305acc84
Sam Praneis <sampraneis@gmail.com>
2025-03-10 14:26:20 -0500

    refactor + comment CitySearch

--- src/main/java/views/components/sidebar/CitySearch.java
+++ src/main/java/views/components/sidebar/CitySearch.java
@@ -1,6 +1,5 @@
 package views.components.sidebar;
 
-import javafx.animation.PauseTransition;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
@@ -10,8 +9,6 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
@@ -28,6 +25,154 @@ import javafx.util.Duration;
  * Get the component via the {@code CitySearch.component()} method.
  */
 public class CitySearch {
+
+  // Frame for the entire section (search bar + results)
+  BorderPane content;
+
+  // results
+  ListView<City> cityListView;
+
+  // Underlying {@code City} collection
+  ObservableList<City> cityList;
+  FilteredList<City> filteredList;
+
+  // Underlying SQL access class
+  CityData cityData;
+
+  // Search box elements
+  BorderPane searchIcon;
+  TextField searchInput;
+  HBox searchBox;
+
+  /**
+   * {@link CitySearch}
+   * Holds state for searching all US Cities.
+   *
+   */
+  public CitySearch() {
+    cityData = new CityData();
+    initComponents();
+  }
+
+  /**
+   * initialize all components. most components will be added to or modified later
+   * during initialization
+   */
+  public void initComponents() {
+    // Frame for the entire section (search bar + results)
+    content = new BorderPane();
+
+    // results
+    cityListView = new ListView<>();
+
+    // Search box elements
+    searchIcon = new BorderPane();
+    searchInput = new TextField();
+    searchBox = new HBox();
+  }
+
+  /**
+   * get the {@code BorderPane} component
+   *
+   * @return the {@code BorderPane} component where:
+   *         The top is the search box
+   *         The center is the serach results
+   */
+  public BorderPane component() {
+
+    // Attempt to get the city list from {@code cityData}
+    try {
+      this.cityList = cityData.getCityList();
+    } catch (Exception e) {
+
+      // For any exception (SQLException or otherwise), notify the user and continue
+      // with an empty (unusable) list.
+      this.cityList = FXCollections.observableArrayList();
+
+      // we have to delay here otherwise the scene won't be loaded!
+      new NotificationBuilder().ofType(NotificationType.Error)
+          .withMessage("Failed to load city data, city search will not be available").showFor(3)
+          .fireAfter(Duration.seconds(2));
+      ;
+    }
+
+    // build our filtered list
+    filteredList = new FilteredList<>(this.cityList);
+
+    // Load our search icon and set dimensions
+    Image img = new Image("/ui/search.png");
+    ImageView imgView = new ImageView(img);
+    imgView.setFitWidth(30);
+    imgView.setFitHeight(30);
+
+    searchIcon.setCenter(imgView);
+
+    // fill our search box
+    searchBox.getChildren().setAll(searchIcon, searchInput);
+
+    // Intialize our FilteredList and set the cell factory
+    cityListView.setItems(filteredList);
+    cityListView.setCellFactory(new CityCellFactory());
+    cityListView.setEditable(false);
+
+    // Fill our border pane
+    content.setCenter(cityListView);
+    content.setTop(searchBox);
+
+    registerEventHandlers();
+    styleComponents();
+
+    return content;
+  }
+
+  private void styleComponents() {
+
+    content.setPadding(new Insets(10));
+    // style
+    cityListView.getStyleClass().add("search-results");
+
+    // Until the user enters text into the search box, the results should be empty
+    cityListView.setVisible(false);
+
+    // add style classes for our search box
+    searchBox.getStyleClass().add("search-box");
+    searchInput.getStyleClass().add("search-input");
+  }
+
+  private void registerEventHandlers() {
+    // Listen to any changes on the input field and adjust the filter
+    // on our listview. This is the core of our "search".
+    //
+    // As an additional side effect, always select the top result
+    searchInput.textProperty().addListener(obs -> {
+      String filter = searchInput.getText();
+      if (filter == null || filter.length() == 0) {
+        cityListView.setVisible(false);
+        filteredList.setPredicate(s -> false);
+      } else {
+        cityListView.setVisible(true);
+        filteredList.setPredicate(city -> city.display.contains(filter));
+      }
+      if (!filteredList.isEmpty()) {
+        cityListView.getSelectionModel().select(0);
+      }
+    });
+
+    searchInput.setOnAction(e -> {
+      City c = cityListView.getSelectionModel().getSelectedItem();
+
+      if (c == null) {
+        searchInput.clear();
+        return;
+      }
+
+      System.out.printf("%s is located at %f, %f\n Updating location\n", c.cityName, c.lat,
+          c.lon);
+
+      content.fireEvent(new LocationChangeEvent(c.lat, c.lon));
+      searchInput.clear();
+    });
+  }
 
   /**
    * A factory that can control the elements displayed by a {@code ListView<City>}
@@ -95,13 +240,13 @@ public class CitySearch {
               setOnMouseClicked(e -> {
                 City c = getListView().getItems().get(this.getIndex());
                 if (c == null) {
-                  inputField.clear();
+                  searchInput.clear();
                   return;
                 }
                 System.out.printf("%s is located at %f, %f\n Updating location\n", c.cityName, c.lat,
                     c.lon);
                 content.fireEvent(new LocationChangeEvent(c.lat, c.lon));
-                inputField.clear();
+                searchInput.clear();
               });
 
               // set the graphic for this cell
@@ -119,124 +264,5 @@ public class CitySearch {
       };
 
     }
-  }
-
-  // Frame for the entire section (search bar + results)
-  BorderPane content;
-
-  // results
-  ListView<City> items;
-
-  // Underlying {@code City} collection
-  ObservableList<City> cityList;
-
-  // Underlying SQL access class
-  CityData cityData;
-
-  // Search box elements
-  BorderPane searchIcon;
-  TextField inputField;
-  HBox inputBox;
-
-  /*
-   * Create a new
-   */
-  public CitySearch() {
-    cityData = new CityData();
-    initComponents();
-  }
-
-  public void initComponents() {
-    // Frame for the entire section (search bar + results)
-    content = new BorderPane();
-
-    // results
-    items = new ListView<>();
-
-    // Search box elements
-    searchIcon = new BorderPane();
-    inputField = new TextField();
-    inputBox = new HBox();
-  }
-
-  public BorderPane component() {
-
-    // Attempt to get the city list from {@code cityData}
-    try {
-      this.cityList = cityData.getCityList();
-    } catch (Exception e) {
-
-      // For any exception (SQLException or otherwise), notify the user and continue
-      // with an empty (unusable) list.
-      this.cityList = FXCollections.observableArrayList();
-
-      // we have to delay here otherwise the scene won't be loaded!
-      new NotificationBuilder().ofType(NotificationType.Error)
-          .withMessage("Failed to load city data, city search will not be available").showFor(3)
-          .fireAfter(Duration.seconds(2));
-      ;
-    }
-
-    FilteredList<City> filtered = new FilteredList<>(this.cityList);
-
-    // Listen to any changes on the input field and adjust the filter
-    // on our listview. This is the core of our "search".
-    //
-    // As an additional side effect, always select the top result
-    inputField.textProperty().addListener(obs -> {
-      String filter = inputField.getText();
-      if (filter == null || filter.length() == 0) {
-        items.setVisible(false);
-        filtered.setPredicate(s -> false);
-      } else {
-        items.setVisible(true);
-        filtered.setPredicate(city -> city.display.contains(filter));
-      }
-      if (!filtered.isEmpty()) {
-        items.getSelectionModel().select(0);
-      }
-    });
-
-    Image img = new Image("/ui/search.png");
-    ImageView imgView = new ImageView(img);
-
-    imgView.setFitWidth(30);
-    imgView.setFitHeight(30);
-
-    inputField.getStyleClass().add("search-input");
-
-    searchIcon.setCenter(imgView);
-
-    inputBox.getChildren().setAll(searchIcon, inputField);
-    inputBox.getStyleClass().add("search-box");
-
-    items.setItems(filtered);
-    items.setCellFactory(new CityCellFactory());
-    items.setEditable(false);
-
-    items.setVisible(false);
-
-    items.getStyleClass().add("search-results");
-
-    content.setCenter(items);
-    content.setPadding(new Insets(10));
-    content.setTop(inputBox);
-
-    content.setOnKeyPressed(key -> {
-      if (key.getCode() == KeyCode.ENTER) {
-        City c = items.getSelectionModel().getSelectedItem();
-
-        if (c == null) {
-          inputField.clear();
-          return;
-        }
-
-        System.out.printf("%s is located at %f, %f\n Updating location\n", c.cityName, c.lat,
-            c.lon);
-        content.fireEvent(new LocationChangeEvent(c.lat, c.lon));
-        inputField.clear();
-      }
-    });
-    return content;
   }
 }
